ae93a20465bf47e37bf956bc244f77b59d0a6314
Joshua Smith <thewanuki@gmail.com>
2017-12-26 19:35:24 -0800

    Begin to migrate to new structure

--- pandapay/pandapay.py
+++ pandapay/pandapay.py
@@ -5,51 +5,65 @@ def _url(path):
     return 'https://api.pandapay.io/v1' + path
 
 
-def _auth(pandapay_secret):
-    return (pandapay_secret)
+class Client(object):
+	def __init__(self, key):
+        auth = (key)
+		self.customers = CustomerClient(auth)
+		self.donations = DonationClient(auth)
+		self.grants = GrantClient(auth)
 
 
-def create_donation(pandapay_auth, **kwargs):
-    return requests.post(
-        _url('/donations'),
-        data=kwargs,
-        auth=pandapay_auth
-    )
+class DonationClient(object):
+	def __init__(self, auth):
+        self.auth = auth
+
+    def create(self, **kwargs):
+        return requests.post(
+            _url('/donations'),
+            data=kwargs,
+            auth=self.auth
+        )
 
 
-def create_customer(pandapay_auth, **kwargs):
-    return requests.post(
-        _url('/customers'),
-        data=kwargs,
-        auth=pandapay_auth
-    )
+class CustomerClient(object):
+	def __init__(self, auth):
+        self.auth = auth
+
+    def create(self, **kwargs):
+        return requests.post(
+            _url('/customers'),
+            data=kwargs,
+            auth=self.auth
+        )
+
+    def update(self, customer_id, **kwargs):
+        url = _url('/customers/{:d}'.format(customer_id))
+        return requests.put(
+            url,
+            data=kwargs,
+            auth=self.auth
+        )
+
+    def delete(self, customer_id):
+        url = _url('/customers/{:d}'.format(customer_id))
+        return requests.delete(
+            url,
+            auth=self.auth
+        )
 
 
-def update_customer(pandapay_auth, customer_id, **kwargs):
-    url = _url('/customers/{:d}'.format(customer_id))
-    return requests.put(
-        url,
-        data=kwargs,
-        auth=pandapay_auth
-    )
+class GrantClient(object):
+	def __init__(self, auth):
+        self.auth = auth
+    
+    def create(self, donation_id, **kwargs):
+        if donation_id:
+            url = _url('/donations/{:d}/grants'.format(donation_id))
+        else:
+            url = _url('/grants')
 
-
-def delete_customer(pandapay_auth, customer_id):
-    url = _url('/customers/{:d}'.format(customer_id))
-    return requests.delete(
-        url,
-        auth=pandapay_auth
-    )
-
-
-def create_grant(pandapay_auth, donation_id, **kwargs):
-    if donation_id:
-        url = _url('/donations/{:d}/grants'.format(donation_id))
-    else:
-        url = _url('/grants')
-
-    return requests.post(
-        url,
-        data=kwargs,
-        auth=pandapay_auth
-    )
+        return requests.post(
+            url,
+            data=kwargs,
+            auth=self.auth
+        )
